051b1b22ebec949b10dc83ca5c06f1aafd919a6a
Andreas Freund <andreas.freund@codekeepers.de>
2023-07-28 15:55:47 +0200

    Pricing service: Deleted unneeded PricingController and PricingService after adding Spring Data REST. Added Eureka server, pricing service registers itself as microservice.

---
+++ eureka/src/main/java/com/udacity/eureka/EurekaApp.java
@@ -0,0 +1,14 @@
+package com.udacity.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// TODO configure so that: localhost:8762/prices -> pricing-service/prices
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApp.class);
+    }
+}
--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -1,17 +1,44 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
 public class PricingServiceApplication {
+    private static final Map<Long, Price> PRICES = LongStream
+            .range(1, 20)
+            .mapToObj(i -> new Price("USD", randomPrice(), i))
+            .collect(Collectors.toMap(Price::getVehicleId, p -> p));
 
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner initDatabase(PriceRepository repository) {
+        return args -> {
+            for (Map.Entry<Long, Price> entry : PRICES.entrySet()) {
+                repository.save(entry.getValue());
+            }
+        };
+    }
+
+    private static BigDecimal randomPrice() {
+        return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
+                .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
+    }
 }
